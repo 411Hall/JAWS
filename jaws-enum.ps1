618c4b8ad8fa1575401577524f0dc00dfc89f817
James Hall <bam489@gmail.com>
2017-08-17 13:07:56 +0100

    Updated modules
    Added SAM File Permissions, Scheduled Tasks, MuiCache

--- jaws-enum.ps1
+++ jaws-enum.ps1
@@ -1,47 +1,80 @@
-write-host "#################################################################################################"
-write-host "## 				         Windows Enum Script                               ##"
-write-host "#################################################################################################"
-write-host "`n"
-$win_version = Get-WmiObject -class Win32_OperatingSystem
-write-host "Windows Version: "$win_version.caption$win_version.version
-write-host "Architecture: "$env:processor_architecture
-write-host "Hostname: " $env:ComputerName
-write-host "Current User: " $env:username
+Windows Priv Esc Enum
+
+Overall
+	- OS Version #
+	- Hostname #
+	- Current User #
+Drives
+	- HDS #
+	- Mapped network drives #
+	- Mounted devices #
+Network
+	- ipconfig#
+	- route
+	- arp #
+	- firewall #
+	- hosts file #
+	- netstat #
+Users
+	- admin users #
+	- logged in users#
+Folders\Files
+	- World writeable folders # 
+	- World writeable files #
+	- Recent docs #
+	- Interesting files (.rdp, .sql, .bat, .ps1, .vbs, inf, conf, .xml) #
+	- Read\Copy SAM file?
+	- Files\Apps in startup directory
+Processes
+	- Running process (with cmd line) #
+Install Apps
+	- Install progs with version number #
+Scheduled Tasks #
+Unquoted Service Paths #
+Tree\Dir List
+
+
+
+----------
+Command line args
+Better output
 
 
 write-host "`n-------------------------------------------------------------------------------------------------"
-write-host "					Network Information                                       "
+write-host "				   Keyword Search                                       "
 write-host "-------------------------------------------------------------------------------------------------"
-Get-WmiObject Win32_NetworkAdapterConfiguration -filter 'IPEnabled= True' | select IpAddress, DNSDomain, DefaultIPGateway | ft -hidetableheaders -autosize | out-string
-
+$lastmodified = get-childitem C:\
+foreach ($modified in $lastmodified){
+    try {
+        Get-ChildItem -Recurse "C:\$folder" -EA SilentlyContinue  | Select-String -pattern "password|admin" | group path | select name
+         }
+    catch {
+        write "Failed to read more files"
+    }
+    }
+    
+    write-host "`n-------------------------------------------------------------------------------------------------"
+write-host "				   10 Last Modified Files                                       "
+write-host "-------------------------------------------------------------------------------------------------"
+$lastmodified = get-childitem C:\Users
+foreach ($modified in $lastmodified){
+    try {
+        Get-ChildItem -Recurse "C:\$folder" -EA SilentlyContinue  | Sort {$_.LastWriteTime} | select -last 10
+         }
+    catch {
+        write "Failed to read more files"
+    }
+    }
 
 write-host "`n-------------------------------------------------------------------------------------------------"
-write-host "					NetStat                                       "
+write-host "				   SAM File Permissions                                       "
 write-host "-------------------------------------------------------------------------------------------------"
-netstat -ano
+
+get-acl "$env:SYSTEMROOT\System32\config\SAM" -EA SilentlyContinue | select path -expand access
+get-acl "$env:SYSTEMROOT\repair\SAM" -EA SilentlyContinue
+get-acl "$env:SYSTEMROOT\System32\config\RegBack\SAM" -EA SilentlyContinue
 
 
-write-host "`n-------------------------------------------------------------------------------------------------"
-write-host "					Users                                       "
-write-host "-------------------------------------------------------------------------------------------------"
-$adsi = [ADSI]"WinNT://$env:COMPUTERNAME"
-$adsi.Children | where {$_.SchemaClassName -eq 'user'} | Foreach-Object {
-    $groups = $_.Groups() | Foreach-Object {$_.GetType().InvokeMember("Name", 'GetProperty', $null, $_, $null)}
-    write-host "----------"
-    write-host "Username: " $_.Name 
-    write-host "Groups:   " $groups
-}
-
-
-write-host "`n-------------------------------------------------------------------------------------------------"
-write-host "					Processes                                       "
-write-host "-------------------------------------------------------------------------------------------------"
-Get-WmiObject win32_process | Select-Object Name,ProcessID,@{n='Owner';e={$_.GetOwner().User}},CommandLine | sort name | format-table -wrap -autosize | out-string
-
-
-write-host "`n-------------------------------------------------------------------------------------------------"
-write-host "					Files with Full Control and Modify Access                                   "
-write-host "-------------------------------------------------------------------------------------------------"
 $files = get-childitem C:\
 foreach ($file in $files){
     try {
@@ -54,101 +87,4 @@ foreach ($file in $files){
     }
     }
     
-write-host "`n-------------------------------------------------------------------------------------------------"
-write-host "				   Folders with Full Control and Modify Access                                       "
-write-host "-------------------------------------------------------------------------------------------------"
-$folders = get-childitem C:\
-foreach ($folder in $folders){
-    try {
-        Get-ChildItem -Recurse "C:\$folder" -EA SilentlyContinue | ?{ $_.PSIsContainer} | get-acl  | select path -expand access |  
-        where {$_.identityreference -notmatch "BUILTIN|NT AUTHORITY|CREATOR OWNER|NT SERVICE"}  | where {$_.filesystemrights -match "FullControl|Modify"} | 
-        select path,filesystemrights,IdentityReference |  ft @{Label="";Expression={Convert-Path $_.Path}}  -hidetableheaders -autosize
-         }
-    catch {
-        write "Failed to read more folders"
-    }
-    }
     
-
-write-host "`n-------------------------------------------------------------------------------------------------"
-write-host "					Mapped Drives                                    "
-write-host "-------------------------------------------------------------------------------------------------"
-Get-WmiObject -Class Win32_LogicalDisk | select DeviceID, VolumeName | ft -hidetableheaders -autosize | out-string
-
-
-write-host "`n-------------------------------------------------------------------------------------------------"
-write-host "					Unquoted Service Path                                   "
-write-host "-------------------------------------------------------------------------------------------------"
-cmd /c  'wmic service get name,displayname,pathname,startmode |findstr /i "auto" |findstr /i /v "c:\windows\\" |findstr /i /v """'
-
-
-write-host "`n-------------------------------------------------------------------------------------------------"
-write-host "					Recent Documents                                  "
-write-host "-------------------------------------------------------------------------------------------------"
-get-childitem "C:\Users\$env:username\AppData\Roaming\Microsoft\Windows\Recent" | select Name | ft -hidetableheaders | out-string
-
-
-write-host "`n-------------------------------------------------------------------------------------------------"
-write-host "					Installed Applications                                  "
-write-host "-------------------------------------------------------------------------------------------------"
-get-wmiobject -Class win32_product | select Name, Version, Caption | ft -hidetableheaders -autosize|  out-String
-
-
-write-host "`n-------------------------------------------------------------------------------------------------"
-write-host "					Installed Pacthes                                  "
-write-host "-------------------------------------------------------------------------------------------------"
-Get-Wmiobject -class Win32_QuickFixEngineering -namespace "root\cimv2" | select-object -Property HotFixID | Out-String
-
-
-write-host "`n-------------------------------------------------------------------------------------------------"
-write-host "					Potentially Vulnerable Services                                "
-write-host "-------------------------------------------------------------------------------------------------"
-#Stolen from https://helvick.blogspot.co.uk/2007/08/checking-service-permissions-with.html
-$services = get-wmiobject -query 'select * from win32_service'
-foreach ($service in $services) {
-    $path=$Service.Pathname
-    if (-not( test-path $path -ea silentlycontinue)) {
-        if ($Service.Pathname -match "(\""([^\""]+)\"")|((^[^\s]+)\s)|(^[^\s]+$)") {
-            $path = $matches[0] –replace """",""
-        }
-    }
-    if (test-path "$path") {
-        $ServiceName = $service.Displayname
-        $secure=get-acl $path
-        foreach ($item in $secure.Access) {
-            if ( ($item.IdentityReference -match "NT AUTHORITY\\SYSTEM"   ) -or
-                 ($item.IdentityReference -match "NT AUTHORITY\\NETWORK"  ) -or
-                 ($item.IdentityReference -match "BUILTIN\\Administrators") -or
-                 ($item.IdentityReference -match "NT SERVICE\\TrustedInstaller") -or
-                ($item.IdentityReference -match "BUILTIN\\Power Users"   ) ) {
-            } else {         
-                if ($item.FileSystemRights.tostring() -match "Modify|Full|Change") {
-                    write-host "------------"
-                    write-host $ServiceName 
-                    write-host $item.IdentityReference.value 
-                    write-host $item.AccessControlType.tostring() $item.FileSystemRights.tostring()
-                }
-            }
-        }
-     }
-}
-write-host "`n-------------------------------------------------------------------------------------------------"
-write-host "					Custom Scheduled Tasks                                "
-write-host "-------------------------------------------------------------------------------------------------"
-#Stolen from https://serverfault.com/questions/604673/how-to-print-out-information-about-task-scheduler-in-powershell-script
-$sched = New-Object -Com "Schedule.Service"
-$sched.Connect()
-$out = @()
-$sched.GetFolder("\").GetTasks(0) | % {
-    $xml = [xml]$_.xml
-    $out += New-Object psobject -Property @{
-        "Name" = $_.Name
-        "Status" = switch($_.State) {0 {"Unknown"} 1 {"Disabled"} 2 {"Queued"} 3 {"Ready"} 4 {"Running"}}
-        "NextRunTime" = $_.NextRunTime
-        "LastRunTime" = $_.LastRunTime
-        "LastRunResult" = $_.LastTaskResult
-        "Author" = $xml.Task.Principals.Principal.UserId
-        "Created" = $xml.Task.RegistrationInfo.Date
-    }
-}
-write-host $out 
